9937f4c966f63a752904de2808833af84533b0c2
DTomusk <david.tomusk@gmail.com>
2020-04-04 17:38:59 +0100

    Cleaning things up

--- anim.py
+++ anim.py
@@ -16,42 +16,52 @@ origin = [canvas_width/2, canvas_height/2]
 
 # finds the positions of everything in 3d space
 def moveStuff(obj):
-	global orientation
-	orientation = Matrix.rotate(orientation, math.pi/48,math.pi/64,math.pi/32)
-	obj.translate(1,0,0)
-	#obj.scale(1.001)
-	obj.rotate(0,0,0)
+	# test out axis rotation
+	print "does nothing for now"
 
+# takes a poly and produces a new one whose attributes match the 2d coordinates 
 def convert(obj):
 	verts = Matrix.mtimes(conversion,Matrix.mtimes(orientation, obj.verts))
+	for v in verts.data:
+		v[0] += origin[0]
+		v[1] += origin[1]
 	edges = obj.edges
 	center = Matrix.mtimes(conversion,Matrix.mtimes(orientation, obj.center))
+	for c in center.data:
+		c[0] += origin[0]
+		c[1] += origin[1]
 	return Poly(verts, edges, center)
 
-# converts positions to 2d and draws them (should decouple)
 def drawStuff(obj):
+	# clear canvas every frame
 	canvas.delete("all")
 	drawBG()
 	drawAxes()
+	# draw everything in the scene 
 	drawPoly(obj)
 
 def drawBG():
 	canvas.create_rectangle(0, 0, canvas_width, canvas_height, fill="black")
 
 def drawAxes():
+	# here I'm converting manually because I don't expect to be converting many non polys
 	for i in range(0, 3):
 		point = Matrix.mtimes(conversion,orientation).data[i]
 		point[0] *= 200
+		point[0] += origin[0]
 		point[1] *= 200
-		drawLine([0,0], point)
+		point[1] += origin[1]
+		drawLine(origin, point)
 
+# draws a 2x2 rectangle
 def drawPoint(point):
-	canvas.create_rectangle(point[0]+origin[0]-2, point[1]+origin[1]-2, 
-		point[0]+origin[0]+2, point[1]+origin[1]+2, fill="white")
+	canvas.create_rectangle(point[0]-2, point[1]-2, 
+		point[0]+2, point[1]+2, fill="white")
 
 def drawLine(start, end):
-	canvas.create_line(origin[0]+start[0], origin[1]+start[1],
-		origin[0]+end[0], origin[1]+end[1], fill="white")
+	canvas.create_line(start[0], start[1],
+		end[0], end[1], fill="white")
+
 
 def drawPoly(obj):
 	toDraw = convert(obj)
@@ -65,6 +75,7 @@ def drawPoly(obj):
 		end = toDraw.verts.data[e[1]]
 		drawLine(start, end)
 
+# work out what's going on in the scene then render the scene 
 def doStuff(obj):
 	moveStuff(obj)
 	drawStuff(obj)
@@ -74,6 +85,8 @@ def main():
 	# want a list of all objects in the scene to do stuff with 
 	cube = Cube()
 	cube.scale(100)
+	# eventually doStuff will pass a whole scene and draw all of the objects there
+	# but for now a cube is good
 	doStuff(cube)
 	mainloop()
 
