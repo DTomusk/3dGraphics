25e245b899a9001a3a1ed8ff9ac9e252e43dc3eb
DTomusk <david.tomusk@gmail.com>
2020-04-15 15:31:44 +0100

    Quick sort seems to work, just need to make it apply to edges

--- three_dizzle/src/quicksort.rs
+++ three_dizzle/src/quicksort.rs
@@ -1,8 +1,6 @@
 pub fn quicksort(mut data: &mut [f64]) {
-    if data.len() <= 1 {
-        return
-    };
     let mut pivot: usize = data.len()/2;
+    println!("Pivot value: {}", data[pivot]);
     // want to iterate counters either side of the pivot
     let mut first_counter = 0;
     let mut second_counter = pivot + 1;
@@ -16,35 +14,58 @@ pub fn quicksort(mut data: &mut [f64]) {
                 println!("Two elements swapped: {} & {}", &data[first_counter], &data[second_counter]);
                 swap(&mut data, first_counter, second_counter);
                 first_counter+=1;
+                println!("First: {}", first_counter);
                 second_counter+=1;
+                println!("Second: {}", second_counter);
             } else if data[first_counter] > data[pivot] {
                 println!("Nothing for first to swap with, increasing second");
                 second_counter+=1;
+                println!("Second: {}", second_counter);
             } else if data[second_counter] < data[pivot] {
                 println!("Nothing for second to swap with, increasing first");
                 first_counter+=1;
+                println!("First: {}", first_counter);
             } else {
                 println!("No swaps for either, increasing both counters");
                 first_counter += 1;
+                println!("First: {}", first_counter);
                 second_counter += 1;
+                println!("Second: {}", second_counter);
             };
 
         } else if first_counter < pivot {
-            if data[first_counter] > data[pivot] {
-                swap(&mut data, pivot - 1, pivot);
+            println!("First counter in bounds, second not");
+            println!("Data at first: {}", data[first_counter]);
+            println!("Data at pivot: {}", data[pivot]);
+            if (data[first_counter] > data[pivot]) & (first_counter == pivot - 1) {
                 swap(&mut data, pivot, first_counter);
                 pivot -= 1;
+            } else if data[first_counter] > data[pivot] {
+                println!("{:?}", data);
+                swap(&mut data, pivot - 1, pivot);
+                println!("{:?}", data);
+                swap(&mut data, pivot, first_counter);
+                println!("{:?}", data);
+                pivot -= 1;
+                println!("Pivot: {}", pivot);
             } else {
                 first_counter += 1;
+                println!("First: {}", first_counter);
             };
         } else {
-            if (data[second_counter] < data[pivot]) & (first_counter == data.len()) {
-                swap(&mut data, pivot + 1, pivot);
-                swap(&mut data, pivot, first_counter);
+            println!("Second counter in bounds, first not");
+            if (data[second_counter] < data[pivot]) & (second_counter == pivot + 1) {
+                swap(&mut data, pivot, second_counter);
                 pivot += 1;
+            } else if data[second_counter] < data[pivot] {
+                swap(&mut data, pivot + 1, pivot);
+                swap(&mut data, pivot, second_counter);
+                pivot += 1;
+                println!("Pivot: {}", pivot);
             // if one thing needs to be moved hold on to it and wait for the other
             } else {
                 second_counter += 1;
+                println!("Second: {}", second_counter);
             };
         };
 
@@ -67,8 +88,11 @@ pub fn quicksort(mut data: &mut [f64]) {
 
         // that should leave just the case where neither need to be moved and are still in bounds
     };
-    quicksort(&mut data[..pivot]);
-    quicksort(&mut data[pivot..]);
+    println!("Exited while loop");
+    if data.len() >= 2 {
+        quicksort(&mut data[..pivot]);
+        quicksort(&mut data[pivot..]);
+    };
 }
 
 pub fn swap(data: &mut[f64], first: usize, second: usize) {
